6fc396400bb470dc18037d05100442af084edbb0
Jesus Ramos <jesus.ramoslobo@fujitsu.com>
2025-01-30 23:55:15 +0100

    challenge tourManagementService

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.example.tourservice.TravelAgentService;
+import com.example.tourservice.TourManagementService;
 
 public class Application {
 
@@ -11,10 +12,15 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService tourManagementService = context.getBean(TourManagementService.class);
+
+        tourManagementService.createTour("Zoo Tour", 100, true);
+
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
         System.out.println("\n******Explore California Tour Kid Friendly Tours******");
         agent.displayToursBy(true);
+
     }
 }
